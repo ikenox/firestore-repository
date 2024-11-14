78afa826598e9be1a492698409496abafd34d646
naoto.ikeno <ikenox@gmail.com>
2024-11-14 22:58:37 +0900

    update

--- src/sample.test.ts
+++ src/sample.test.ts
@@ -1,6 +1,14 @@
-import { GenericContainer } from 'testcontainers';
+import admin from 'firebase-admin';
+import { getFirestore } from 'firebase-admin/firestore';
 import { it } from 'vitest';
 
 it('test', async () => {
-  const container = await new GenericContainer('').withReuse().start();
+  const db = getFirestore(
+    admin.initializeApp({
+      projectId: 'dummy-project',
+    }),
+  );
+  await db.collection('testCollection').doc('hoge').set({ foo: 123 });
+  const res = await db.collection('testCollection').get();
+  console.log(res.docs.map((d) => d.data()));
 });
