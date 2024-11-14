7a85c22c4fff0acc1c978e0d6d7398d68ff381c3
naoto.ikeno <ikenox@gmail.com>
2024-11-14 23:18:52 +0900

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globalSetup: './src/__test__/globalSetup.ts',
+    env: {
+      FIRESTORE_EMULATOR_HOST: 'localhost:60001',
+    },
   },
 });
