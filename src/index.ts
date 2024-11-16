955b3570e12ac9a733a5222fbf9c7cd9615859b9
naoto.ikeno <ikenox@gmail.com>
2024-11-16 11:18:54 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { type Static, Type } from '@sinclair/typebox';
 import {
   AggregateField,
+  CollectionReference,
   DocumentReference,
+  Filter,
+  OrderByDirection,
   Query,
   Timestamp,
   WhereFilterOp,
@@ -15,32 +18,58 @@ export type Document = {
   [key: string]: ValueType;
 };
 
-export type ValueType =
-  | number
-  | string
-  | null
-  | Timestamp
-  | DocumentReference
-  | ValueType[]
-  | { [K in string]: ValueType };
+export type ValueType = number | string | null | Timestamp | DocumentReference | ValueType[] | Map;
+export type Map = { [K in string]: ValueType };
 
-export class TypedQuery<
-  DbModelType extends Document = Document,
-  AppModelType extends Document = DbModelType,
-> extends Query<DbModelType, AppModelType> {
-  $where<T extends keyof AppModelType>(
+type TypedFieldPath<T extends Document> = {
+  [K in keyof T & string]: `${K}` | `${K}.${ValuePaths<T[K]>}`;
+}[keyof T & string];
+type ValuePaths<T extends ValueType> = T extends Map
+  ? {
+      [K in keyof T & string]: `${K}` | `${K}.${ValuePaths<T[K]>}`;
+    }[T & string]
+  : never;
+
+export class TypedQuery<DbModelType extends Document = Document, AppModelType = DbModelType> {
+  constructor(readonly inner: Query<AppModelType, DbModelType>) {}
+
+  where(filter: Filter): TypedQuery<DbModelType, AppModelType>;
+  where<T extends AppModelType>(
     fieldPath: T,
     opStr: WhereFilterOp,
     value: AppModelType[T], // TODO change type per operator
-  ): TypedQuery<DbModelType, AppModelType> {}
+  ): TypedQuery<DbModelType, AppModelType>;
+  where<T extends keyof AppModelType>(
+    fieldPathOrFilter: Filter | T,
+    opStr?: WhereFilterOp,
+    value?: AppModelType[T],
+  ): TypedQuery<DbModelType, AppModelType> {
+    if (fieldPathOrFilter instanceof Filter) {
+      return new TypedQuery(this.inner.where(fieldPathOrFilter));
+    }
+    return new TypedQuery(this.inner.where(fieldPathOrFilter, opStr, value));
+  }
 
-  $select<T extends (keyof AppModelType)[]>(
-    ...fields: T
-  ): TypedQuery<DbModelType, Pick<AppModelType, T>> {}
+  orderBy<T extends keyof AppModelType>(
+    fieldPath: T,
+    directionStr?: OrderByDirection,
+  ): TypedQuery<DbModelType, AppModelType> {
+    return new TypedQuery<DbModelType, AppModelType>(this.inner.orderBy(fieldPath, directionStr));
+  }
+
+  limit(limit: number): TypedQuery<DbModelType, AppModelType> {
+    return new TypedQuery<DbModelType, AppModelType>(this.inner.limit(limit));
+  }
 }
 
-export const collection = <DbModel extends Document = never>(name: string): TypedQuery<DbModel> =>
-  new TypedQuery<DbModel>(name);
+export class TypedCollectionReference<
+  DbModelType extends Document = Document,
+  AppModelType extends Document = DbModelType,
+> extends TypedQuery<DbModelType, AppModelType> {}
+
+export const collection = <DbModel extends Document = never>(
+  name: string,
+): TypedCollectionReference<DbModel> => new TypedQuery<DbModel>(name);
 
 const users = collection<{
   userId: string;
--- src/sample.test.ts
+++ src/sample.test.ts
@@ -8,16 +8,17 @@ it('test', async () => {
       projectId: 'dummy-project',
     }),
   );
-  // const doc = await db.collection('testCollection').doc('hoge').get();
-  // const res = await db
-  //   .collection('testCollection')
-  //   .where('map', 'array-contains', { aaaa: '123' })
-  //   .get();
-  const collection = db.collection('hoge');
-  const doc = collection.doc();
-  await doc.set({ a: { b: { c: 123 } } });
-  console.log(await doc.get().then((d) => d.data()));
-
-  await doc.update({ a: { b: { d: 123 } } }, { exists: false });
-  console.log(await doc.get().then((d) => d.data()));
+  const doc = await db.collection('testCollection').doc('hoge');
+  await doc.set({
+    a: { b: [1, 2, 3] },
+  });
+  const res = await db.collection('testCollection').where('a.b.1', '==', 2).get();
+  console.log(res.docs.map((d) => d.data()));
+  // const collection = db.collection('hoge');
+  // const doc = collection.doc();
+  // await doc.set({ a: { b: { c: 123 } } });
+  // console.log(await doc.get().then((d) => d.data()));
+  //
+  // await doc.update({ a: { b: { d: 123 } } }, { exists: false });
+  // console.log(await doc.get().then((d) => d.data()));
 });
