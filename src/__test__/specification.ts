c8b8f980798a0d93bf69d9c28cbc1d88e08d3571
naoto.ikeno <ikenox@gmail.com>
2024-11-24 16:07:12 +0900

    update

--- src/__test__/specification.ts
+++ src/__test__/specification.ts
@@ -9,76 +9,78 @@ import { CollectionSchema, Repository, Timestamp, as, collection } from '../inde
 export const defineRepositorySpecificationTests = <T extends Repository>(
   repository: <const T extends CollectionSchema>(collection: T) => Repository<T>,
 ) => {
-  allMethodsTests({
-    title: 'root collection',
-    repository: repository(authorsCollection),
-    initial: [
-      {
-        authorId: 'author0',
-        name: 'name0',
-        registeredAt: AdminTimestamp.fromDate(new Date()),
+  describe('repository specifications', () => {
+    allMethodsTests({
+      title: 'root collection',
+      repository: repository(authorsCollection),
+      initial: [
+        {
+          authorId: 'author0',
+          name: 'name0',
+          registeredAt: AdminTimestamp.fromDate(new Date()),
+        },
+        {
+          authorId: 'author1',
+          name: 'name1',
+          registeredAt: AdminTimestamp.fromDate(new Date()),
+        },
+        {
+          authorId: 'author2',
+          name: 'name2',
+          registeredAt: AdminTimestamp.fromDate(new Date()),
+        },
+      ],
+      newData: () => {
+        const id = randomNumber();
+        return {
+          authorId: `author_${id}`,
+          name: `name_${id}`,
+          registeredAt: AdminTimestamp.fromDate(new Date()),
+        };
       },
-      {
-        authorId: 'author1',
-        name: 'name1',
-        registeredAt: AdminTimestamp.fromDate(new Date()),
-      },
-      {
-        authorId: 'author2',
-        name: 'name2',
-        registeredAt: AdminTimestamp.fromDate(new Date()),
-      },
-    ],
-    newData: () => {
-      const id = randomNumber();
-      return {
-        authorId: `author_${id}`,
-        name: `name_${id}`,
-        registeredAt: AdminTimestamp.fromDate(new Date()),
-      };
-    },
-    mutate: (data) => ({
-      ...data,
-      name: `${data.name}_updated`,
-    }),
-  });
+      mutate: (data) => ({
+        ...data,
+        name: `${data.name}_updated`,
+      }),
+    });
 
-  allMethodsTests({
-    title: 'subcollection',
-    repository: repository(postsCollection),
-    initial: [
-      {
-        postId: 0,
-        title: 'post0',
-        authorId: 'author0',
-        postedAt: AdminTimestamp.fromDate(new Date()),
+    allMethodsTests({
+      title: 'subcollection',
+      repository: repository(postsCollection),
+      initial: [
+        {
+          postId: 0,
+          title: 'post0',
+          authorId: 'author0',
+          postedAt: AdminTimestamp.fromDate(new Date()),
+        },
+        {
+          postId: 1,
+          title: 'post1',
+          authorId: 'author0',
+          postedAt: AdminTimestamp.fromDate(new Date()),
+        },
+        {
+          postId: 2,
+          title: 'post2',
+          authorId: 'author1',
+          postedAt: AdminTimestamp.fromDate(new Date()),
+        },
+      ],
+      newData: () => {
+        const id = randomNumber();
+        return {
+          postId: id,
+          title: `post${id}`,
+          authorId: `author${id}`,
+          postedAt: AdminTimestamp.fromDate(new Date()),
+        };
       },
-      {
-        postId: 1,
-        title: 'post1',
-        authorId: 'author0',
-        postedAt: AdminTimestamp.fromDate(new Date()),
-      },
-      {
-        postId: 2,
-        title: 'post2',
-        authorId: 'author1',
-        postedAt: AdminTimestamp.fromDate(new Date()),
-      },
-    ],
-    newData: () => {
-      const id = randomNumber();
-      return {
-        postId: id,
-        title: `post${id}`,
-        authorId: `author${id}`,
-        postedAt: AdminTimestamp.fromDate(new Date()),
-      };
-    },
-    mutate: (data) => ({
-      ...data,
-      title: `${data.title}_updated`,
-    }),
+      mutate: (data) => ({
+        ...data,
+        title: `${data.title}_updated`,
+      }),
+    });
   });
 };
 
@@ -91,7 +93,7 @@ export const allMethodsTests = <T extends Repository>(params: {
 }) => {
   const repository = params.repository;
 
-  describe('repository specifications', async () => {
+  describe(params.title, async () => {
     beforeEach(async () => {
       await deleteAll(repository, {});
       await repository.batchSet(params.initial);
