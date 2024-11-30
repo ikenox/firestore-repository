aecd270c954e6d23d37587e09bf1ddea10a94560
naoto.ikeno <ikenox@gmail.com>
2024-12-01 00:16:21 +0900

    update

--- packages/google-cloud-firestore/src/repository.ts
+++ packages/google-cloud-firestore/src/repository.ts
@@ -5,7 +5,7 @@ import {
   type Query as FirestoreQuery,
   Transaction,
   type WriteBatch,
-} from 'firebase-admin/firestore';
+} from '@google-cloud/firestore';
 import {
   type DbModel,
   type Id,
