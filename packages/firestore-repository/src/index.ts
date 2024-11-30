f5f4a00e6ca729186a83bb08ae4b5c72c9f031d1
naoto.ikeno <ikenox@gmail.com>
2024-12-01 00:14:55 +0900

    update

--- packages/firestore-repository/src/index.ts
+++ packages/firestore-repository/src/index.ts
@@ -1,7 +1,7 @@
 import type * as sdk from '@firebase/firestore';
 import type * as admin from 'firebase-admin/firestore';
-import type { Prettify } from '../../../src/util.js';
 import type { Query, QueryConstraint } from './query.js';
+import type { Prettify } from './util.js';
 
 /**
  * An entrypoint of schema definition
@@ -130,7 +130,7 @@ export interface Repository<
     error?: (error: Error) => void,
   ) => Unsubscribe;
 
-  list: (query: Query<T>) => Promise<Model<T>[]>;
+  list: (query: Query<T, Env>) => Promise<Model<T>[]>;
 
   /**
    * Listen documents by the specified query
