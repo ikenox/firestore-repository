9f52a1e3132b1b7dc708196171f25c678263c9b3
naoto.ikeno <ikenox@gmail.com>
2024-12-01 00:33:39 +0900

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,17 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
-// biome-ignore lint/style/noDefaultExport: <explanation>
+// biome-ignore lint/style/noDefaultExport:
 export default defineConfig({
   test: {
     include: ['**/*.test.ts'],
-    globalSetup: './src/__test__/setup.ts',
     isolate: false,
-    pool: 'threads',
     coverage: {
       provider: 'v8',
       reporter: ['text'],
+    },
+    alias: {
+      'firestore-repository': path.join(import.meta.dirname, 'packages/firestore-repository/src'),
     },
   },
 });
---
+++ vitest.workspace.ts
@@ -0,0 +1,4 @@
+import { defineWorkspace } from 'vitest/config';
+
+// biome-ignore lint/style/noDefaultExport:
+export default defineWorkspace(['packages/*']);
