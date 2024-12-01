306e8774d29ebb74befa8a259c8efbfd505d38da
naoto.ikeno <ikenox@gmail.com>
2024-12-02 00:36:27 +0900

    update

--- packages/firestore-repository/src/index.test.ts
+++ packages/firestore-repository/src/index.test.ts
@@ -55,37 +55,41 @@ describe('CollectionSchema', () => {
   type Authors = typeof authorsCollection;
   type Posts = typeof postsCollection;
 
-  it('type', () => {
-    // root collection
-    expectTypeOf<Id<Authors>>().toEqualTypeOf<{ authorId: string }>();
-    expectTypeOf<ParentId<Authors>>().toEqualTypeOf<Record<never, never>>();
-    expectTypeOf<Model<Authors>>().toEqualTypeOf<{
-      authorId: string;
-      name: string;
-      registeredAt: Date;
-    }>();
-    expectTypeOf<DbModel<Authors>>().toEqualTypeOf<{
-      authorId: string;
-      name: string;
-      registeredAt: Timestamp;
-    }>();
+  describe('schema types', () => {
+    it('root collection', () => {
+      expectTypeOf<Id<Authors>>().toEqualTypeOf<{ authorId: string }>();
+      expectTypeOf<ParentId<Authors>>().toEqualTypeOf<Record<never, never>>();
+      expectTypeOf<Model<Authors>>().toEqualTypeOf<{
+        authorId: string;
+        name: string;
+        registeredAt: Date;
+      }>();
+      expectTypeOf<DbModel<Authors>>().toEqualTypeOf<{
+        authorId: string;
+        name: string;
+        registeredAt: Timestamp;
+      }>();
+    });
 
-    // subcollection
-    expectTypeOf<Id<Posts>>().toEqualTypeOf<{ postId: number; authorId: string }>();
-    expectTypeOf<ParentId<Posts>>().toEqualTypeOf<{ authorId: string }>();
-    expectTypeOf<Model<Posts>>().toEqualTypeOf<{
-      postId: number;
-      authorId: string;
-      title: string;
-      postedAt: Date;
-    }>();
-    expectTypeOf<DbModel<Posts>>().toEqualTypeOf<{
-      postId: number;
-      authorId: string;
-      title: string;
-      postedAt: Timestamp;
-    }>();
+    it('subcollection', () => {
+      expectTypeOf<Id<Posts>>().toEqualTypeOf<{ postId: number; authorId: string }>();
+      expectTypeOf<ParentId<Posts>>().toEqualTypeOf<{ authorId: string }>();
+      expectTypeOf<Model<Posts>>().toEqualTypeOf<{
+        postId: number;
+        authorId: string;
+        title: string;
+        postedAt: Date;
+      }>();
+      expectTypeOf<DbModel<Posts>>().toEqualTypeOf<{
+        postId: number;
+        authorId: string;
+        title: string;
+        postedAt: Timestamp;
+      }>();
+    });
+  });
 
+  it('write model', () => {
     expectTypeOf<WriteValue<string>>().toEqualTypeOf<string>();
     expectTypeOf<WriteValue<{ a: { b: 123 } }>>().toEqualTypeOf<{ a: { b: 123 } }>();
     expectTypeOf<WriteValue<Timestamp>>().toEqualTypeOf<Date | Timestamp>();
