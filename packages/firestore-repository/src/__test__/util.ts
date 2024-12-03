3b2b8bd14aedc465b82705aad45a84588a1139c8
naoto.ikeno <ikenox@gmail.com>
2024-12-03 16:46:04 +0900

    update

--- packages/firestore-repository/src/__test__/util.ts
+++ packages/firestore-repository/src/__test__/util.ts
@@ -1,3 +1,3 @@
 export const randomNumber = () => 1000000 + Math.floor(Math.random() * 1000000);
 
-export const randomString = () => Math.random().toString(36).slice(-16);
+export const randomString = () => Math.random().toString(36).slice(2);
