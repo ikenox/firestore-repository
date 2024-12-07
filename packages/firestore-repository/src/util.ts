945dee1c1483e8ed5b19db77fb5c5068fec3d15c
naoto.ikeno <ikenox@gmail.com>
2024-12-07 12:27:00 +0900

    update

--- packages/firestore-repository/src/util.ts
+++ packages/firestore-repository/src/util.ts
@@ -1,2 +1,6 @@
 export type Prettify<T> = T extends unknown ? { [K in keyof T]: T[K] } : never;
 export const prettify = <T>(obj: T): Prettify<T> => obj as Prettify<T>;
+
+export const assertNever = (x: never): never => {
+  throw new Error(`This code should be unreached but: ${x}`);
+};
