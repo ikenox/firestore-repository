3dfb8fcef19dbbcfa3fddc01a4af3f88d57b170a
naoto.ikeno <ikenox@gmail.com>
2024-12-08 01:22:46 +0900

    update

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vitest/config';
 export const sharedConfig = defineConfig({
   test: {
     include: ['**/*.test.ts'],
+    hookTimeout: 500,
+    testTimeout: 500,
     alias: {
       'firestore-repository': path.join(import.meta.dirname, 'packages/firestore-repository/src'),
     },
