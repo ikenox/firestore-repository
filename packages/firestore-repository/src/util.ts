bb9def4557515f7b669fba8693b5ed59ec33d3f1
naoto.ikeno <ikenox@gmail.com>
2024-12-11 23:45:30 +0900

    update

--- packages/firestore-repository/src/util.ts
+++ packages/firestore-repository/src/util.ts
@@ -1,5 +1,6 @@
 /**
- * A utility method
+ * Receives and returns `never` value
+ * This is mainly used for exhaustiveness check of a switch statement
  */
 export const assertNever = (x: never): never => {
   throw new Error(`This code should be unreached but: ${x}`);
